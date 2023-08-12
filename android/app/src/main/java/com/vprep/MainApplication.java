8aa66ce0c5d8f85c629f50cc38e3d43fefbb8c52
krishna415741 <krishna.garaga@neosoftmail.com>
2023-08-12 16:28:01 +0530

    fixed build after upgrading to latest react nativ epackage

--- android/app/src/main/java/com/vprep/MainApplication.java
+++ android/app/src/main/java/com/vprep/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 import com.oblador.vectoricons.VectorIconsPackage;
-
+import com.rnfs.RNFSPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
